f053c03b3d8dbf00d6e2cdf373acff10ae73ac31
Arya Dwi Putra <aryadptr.business@gmail.com>
2023-11-14 10:34:55 +0700

    configure react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,12 @@ import App from "./App.jsx";
 
 import "./assets/style/main.css";
 
+import { BrowserRouter } from "react-router-dom";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
